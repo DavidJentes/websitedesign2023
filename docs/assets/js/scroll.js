f62d8b7a42e84aa718090c15443a0368b9492d61
DavidDGTNT <29644042+dugo3number2@users.noreply.github.com>
2023-01-18 21:44:59 +0000

    Finish site styling

--- docs/assets/js/scroll.js
+++ docs/assets/js/scroll.js
@@ -1,11 +1,27 @@
 window.onscroll = () => {
-    headerScroll()
+	headerScroll()
 }
 
+const header = document.getElementsByTagName("header")[0]
+const headerText = document.getElementsByTagName("h1")[0]
+const headerButtons = document.getElementsByTagName("ul")[0].children
+
 function headerScroll() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementsByTagName("header")[0].children[0].style.fontSize = "16px"
-    } else {
-        document.getElementsByTagName("header")[0].children[0].style.fontSize = "32px"
-    }
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		header.style.padding = "5px"
+		headerText.style.fontSize = "16px"
+		headerText.style.padding = "5px"
+		for (const button of headerButtons) {
+			button.style.fontSize = "12px"
+			button.children[0].style.padding = "15px"
+		}
+	} else {
+		header.style.padding = "10px"
+		headerText.style.fontSize = "32px"
+		headerText.style.padding = "10px"
+		for (const button of headerButtons) {
+			button.style.fontSize = "16px"
+			button.children[0].style.padding = "20px"
+		}
+	}
 }
