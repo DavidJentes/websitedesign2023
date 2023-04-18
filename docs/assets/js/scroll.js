f715b34e3355064817de8767ff3311d9cd54d6ac
dugo3number2 <daviddgtntbackup@outlook.com>
2023-04-18 12:44:33 -0500

    Add logo to pages

--- docs/assets/js/scroll.js
+++ docs/assets/js/scroll.js
@@ -3,12 +3,15 @@ window.onscroll = () => {
 }
 
 const header = document.getElementsByTagName("header")[0]
+const logo = document.getElementsByClassName("logo")[0]
 const headerText = document.getElementsByTagName("h1")[0]
 const headerButtons = document.getElementsByTagName("ul")[0].children
 
 function headerScroll() {
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 		header.style.padding = "5px"
+		logo.style.width = "50px"
+		logo.style.height = "50px"
 		headerText.style.fontSize = "16px"
 		headerText.style.padding = "5px"
 		for (const button of headerButtons) {
@@ -17,6 +20,8 @@ function headerScroll() {
 		menuButton.style.width = "20px"
 	} else {
 		header.style.padding = "10px"
+		logo.style.width = "75px"
+		logo.style.height = "75px"
 		headerText.style.fontSize = "32px"
 		headerText.style.padding = "10px"
 		for (const button of headerButtons) {
